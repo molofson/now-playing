26547da34e53f57114c79c313e7e13299faea0f5
Mike Olofson <github@olofson.info>
2025-09-11 19:42:54 -0500

    cleanup

--- test_linting.py
+++
@@ -1,8 +0,0 @@
-def test_function(used_arg, unused_arg):  # noqa: U100
-    """Test function with an unused argument."""
-    return used_arg
-
-
-def another_test(arg1, arg2):  # noqa: U100
-    """Another function that doesn't use arg2."""
-    print(arg1)
--- test_no_line_limit.py
+++
@@ -1,3 +0,0 @@
-def test_very_long_line():
-    """This is a very long line that definitely exceeds 120 characters and should NOT be caught by flake8 now that we have unlimited line length configured again."""
-    return True
